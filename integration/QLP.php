01287e07f8c6f17cbe8292adaadefc1d2e1b8caf
Radius17 <radius17@gmail.com>
2014-10-04 14:26:01 +0400

    Поправки плагина QLP

--- integration/QLP.php
+++ integration/QLP.php
@@ -10,34 +10,35 @@
  *	@link http://kcfinder.sunhater.com
  */
 class QLP{
-    protected static $authenticated = false;
-    static function checkAuth() {
-        $current_cwd = getcwd();
-        if (!self::$authenticated) {
-        	define('_QUICK_LP', 'PL_KCIUQ_');
-            $init_0 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/config/config.php");
-        	$init_1 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPRegistry.php");
-            $init_2 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPSimpleObject.php");
-        	$init_3 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPSession.php");
-        	$data_path=realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/data/");
-            include_once($init_0);
-            include_once($init_1);
-            include_once($init_2);
-            include_once($init_3);
-            if(QLPSession::getInstance()->isLoggedIn()){
-            	self::$authenticated = true;
-            	$_SESSION['KCFINDER']=null;
-                if(!isset($_SESSION['KCFINDER'])) $_SESSION['KCFINDER'] = array();
-                if(!isset($_SESSION['KCFINDER']['disabled'])) $_SESSION['KCFINDER']['disabled'] = false;
-                $_SESSION['KCFINDER']['_check4htaccess'] = false;
-                $_SESSION['KCFINDER']['uploadURL'] = '/data/';
-                $_SESSION['KCFINDER']['uploadDir'] = $data_path;
-                $_SESSION['KCFINDER']['theme'] = 'default';
-                $_SESSION['KCFINDER']['types'] = array('gallery'   =>  "",'media'   =>  "");
-            }
-        }
-        chdir($current_cwd);
-        return self::$authenticated;
-    }
+	protected static $authenticated = false;
+	static function checkAuth() {
+		$current_cwd = getcwd();
+		if (!self::$authenticated) {
+			define('_QUICK_LP', 'PL_KCIUQ_');
+			$init_0 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/config/config.php");
+			$init_1 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPRegistry.php");
+			$init_2 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPSimpleObject.php");
+			$init_3 = realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/engine/core/QLPSession.php");
+			$site_path=realpath(dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/");
+			include_once($init_0);
+			include_once($init_1);
+			include_once($init_2);
+			include_once($init_3);
+			if(QLPSession::getInstance()->isLoggedIn()){
+				self::$authenticated = true;
+					$_SESSION['KCFINDER']=null;
+				if(!isset($_SESSION['KCFINDER'])) $_SESSION['KCFINDER'] = array();
+				if(!isset($_SESSION['KCFINDER']['disabled'])) $_SESSION['KCFINDER']['disabled'] = false;
+				$_SESSION['KCFINDER']['_check4htaccess'] = false;
+				$_SESSION['KCFINDER']['uploadURL'] = '/';
+				$_SESSION['KCFINDER']['uploadDir'] = $site_path;
+				$_SESSION['KCFINDER']['theme'] = 'default';
+				$_SESSION['KCFINDER']['thumbsDir'] = "/data/.thumbs";
+				$_SESSION['KCFINDER']['types'] = array('data'   =>  "");
+			}
+		}
+		chdir($current_cwd);
+		return self::$authenticated;
+	}
 }
 QLP::checkAuth();
